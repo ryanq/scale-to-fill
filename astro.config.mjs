b59d7859c2f73f8c7edd4050f9f7eccefe0ea5fc
Ryan Quattlebaum <ryan.quattlebaum@icloud.com>
2023-08-22 15:34:56 -0400

    Add deploy workflow script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ryanq.github.io',
+  base: '/scale-to-fill',
   integrations: [svelte()]
 });
